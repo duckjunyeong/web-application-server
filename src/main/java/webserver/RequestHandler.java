d5bdd8e71df4f5aeadbe618de84d12b56d60744b
junyeong <whwnsdud4592@naver.com>
2025-04-10 08:35:35 +0900

    feat : 1단계 구현 - index.html파일 반환하기

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,16 +1,19 @@
 package webserver;
 
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
+    private static final String STATIC_FORDLER = "webapp/";
+    private static final String INDEX_PAGE = "/";
 
     private Socket connection;
 
@@ -24,13 +27,56 @@ public class RequestHandler extends Thread {
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
-            DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello World".getBytes();
-            response200Header(dos, body.length);
-            responseBody(dos, body);
+
+            byte[] buffer = new byte[1024];
+            int byteRead = in.read(buffer);
+            String strBuffer = new String(buffer, StandardCharsets.UTF_8);
+            String[] requestMethodAndPath = extractMethodAndPath(strBuffer);
+
+            String requestMethod = requestMethodAndPath[0];
+            String requestPath = requestMethodAndPath[1];
+
+            generateResponse(out, requestMethod, requestPath);
         } catch (IOException e) {
             log.error(e.getMessage());
         }
+    }
+
+    private void generateResponse(OutputStream out, String requestMethod, String requestPath) throws IOException {
+        DataOutputStream dos = new DataOutputStream(out);
+        if (requestMethod.equals("GET")){
+            if (requestPath.equals(INDEX_PAGE)){
+                indexPage(dos);
+            }
+            else{
+                errorPage(dos);
+            }
+        }
+        throw new IllegalArgumentException("Invalid Path");
+    }
+
+    private String[] extractMethodAndPath(String strBuffer) {
+        String[] splitedBuffer = strBuffer.split("\\n");
+        return splitedBuffer[0].split(" ");
+    }
+
+    private void indexPage(DataOutputStream dos) throws IOException {
+        Path path = Paths.get(STATIC_FORDLER + "index.html");
+        if (Files.exists(path)){
+            byte[] htmlBytes = Files.readAllBytes(path);
+            handleResponse(dos, htmlBytes);
+        }
+        throw new IllegalArgumentException("Invalid file");
+    }
+
+    private void errorPage(DataOutputStream dos) {
+        byte[] body = "Hello World".getBytes();
+        handleResponse(dos, body);
+    }
+
+    private void handleResponse(DataOutputStream dos, byte[] bytes) {
+        response200Header(dos, bytes.length);
+        responseBody(dos, bytes);
     }
 
     private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
